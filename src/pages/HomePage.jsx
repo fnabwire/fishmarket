bc1be64d838202c04505a44ae6ba1bbd80c83314
fnabwire <fransiscanabwire2@gmail.com>
2025-04-01 02:07:46 +0300

    Revert "Merge branch 'main' of https://github.com/fnabwire/fishmarket"
    This reverts commit 7101fe21235f9632f8cf357ad8b6fb1976f60f6d, reversing
    changes made to 115f1e33dcf468899525819e6894f5f95db8bb68.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import FishListings from '../components/FishListings'
+import Footer from '../components/Footer' 
 
 function HomePage() {
   return (
     <div>
       <Navbar />
       <HeroSection />
-      <FishListings />  
+      <FishListings />
+      <Footer />  
     </div>
   )
 }
