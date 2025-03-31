b6fa23bd5af57a251f0004a155c92c82e215e54e
seifRashid <rseif121@gmail.com>
2025-04-01 01:26:20 +0300

    Create fish listing

--- src/components/FishListings.jsx
+++ src/components/FishListings.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 
 function FishListings() {
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Fish Listings</h1>
-      <p className="text-muted-foreground">Explore our wide variety of fish available for sale.</p>
-    </div>
+    <>
+    <h1>Popular Fish Listings</h1>
+    </>
   )
 }
 
